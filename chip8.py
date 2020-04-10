a9e558b552f6921856d7f2c8fc9be66889d132d1
Rene Delgado <rjdelgadoc@falabella.cl>
2020-04-10 17:50:20 -0400

    Add Display and bug fixes

--- chip8.py
+++ chip8.py
@@ -1,4 +1,8 @@
+import sys
+
+import pygame
 import numpy as np
+from pygame.locals import *
 
 class Cpu(object):
 
@@ -8,7 +12,8 @@ class Cpu(object):
     _DISPLAY_SIZE = (64,32)
     _N_KEYS = 16
 
-    def __init__(self):
+    def __init__(self, display):
+        self.display = display
         # Registers
         # General purpose registers
         self.reg_V = np.zeros(shape=self._N_REGISTERS, dtype=np.uint8)
@@ -95,36 +100,36 @@ class Cpu(object):
             self.reg_V[x] += kk
         elif instruction & 0xF000 == 0x8000:
             # 8xy0 - LD Vx, Vy - Set Vx = Vy
-            if n = 0x0:
+            if n == 0x0:
                 self.reg_V[x] = self.reg_V[y]
             # 8xy1 - OR Vx, Vy - Set Vx = Vx OR Vy
-            elif n = 0x1:
+            elif n == 0x1:
                 self.reg_V[x] |= self.reg_V[y]
             # 8xy2 - AND Vx, Vy - Set Vx = Vx AND Vy
-            elif n = 0x2:
+            elif n == 0x2:
                 self.reg_V[x] &= self.reg_V[y]
             # 8xy3 - XOR Vx, Vy - Set Vx = Vx XOR Vy
-            elif n = 0x3:
+            elif n == 0x3:
                 self.reg_V[x] ^= self.reg_V[y]
             # 8xy4 - ADD Vx, Vy - Set Vx = Vx + Vy, set VF = carry
-            elif n = 0x4:
+            elif n == 0x4:
                 s = np.uint16(self.reg_V[x]) + np.uint16(self.reg_V[y])
                 self.reg_V[0xF] = (s) > np.uint16(0xFF);
                 self.reg_V[x] = np.uint8(s)
             # 8xy5 - SUB Vx, Vy - Set Vx = Vx - Vy, set VF = NOT borrow
-            elif n = 0x5:
+            elif n == 0x5:
                 self.reg_V[0xF] = self.reg_V[x] > self.reg_V[y]
                 self.reg_V[x] -= self.reg_V[y]
             # 8xy6 - SHR Vx {, Vy} - Set Vx = Vx SHR 1
-            elif n = 0x6:
+            elif n == 0x6:
                 self.reg_V[0xF] = (self.reg_V[X] & 0x01);
                 self.reg_V[x] >>= 1;
             # 8xy7 - SUBN Vx, Vy - Set Vx = Vy - Vx, set VF = NOT borrow
-            elif n = 0x7:
+            elif n == 0x7:
                 self.reg_V[0xF] = self.reg_V[y] > self.reg_V[x]
                 self.reg_V[x] = self.reg_V[y] - self.reg_V[x]
             # 8xyE - SHL Vx {, Vy} - Set Vx = Vx SHL 1
-            elif n = 0xE:
+            elif n == 0xE:
                 self.reg_V[0xF] = (self.reg_V[x] >> 7);
                 self.reg_V[x] <<= 1;
         # 9xy0 - SNE Vx, Vy - Skip next instruction if Vx != Vy
@@ -141,55 +146,104 @@ class Cpu(object):
         elif instruction & 0xF000 == 0xC000:
             self.reg_V[x] = np.random.randint(0, 255, dtype=np.uint8) & kk
         # Dxyn - DRW Vx, Vy, nibble - Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
-        elif instruction & 0xF000 == 0xD000
+        elif instruction & 0xF000 == 0xD000:
             self.draw_sprite(self.reg_V[x], self.reg_V[y], n)
             self.update_screen()
         elif instruction & 0xF000 == 0xE000:
             # Ex9E - SKP Vx - Skip next instruction if key with the value of Vx is pressed
-            if n = 0xE:
+            if n == 0xE:
                 if self.key_states[self.reg_V[x]]:
                     self.reg_PC += 2
             # ExA1 - SKNP Vx - Skip next instruction if key with the value of Vx is not pressed
-            elif n = 0x1:
+            elif n == 0x1:
                 if not self.key_states[self.reg_V[x]]:
                     self.reg_PC += 2
         elif instruction & 0xF000 == 0xF000:
             # Fx07 - LD Vx, DT - Set Vx = delay timer value
-            if n = 0x07:
+            if n == 0x07:
                 self.reg_V[x] = self.reg_DT
             # Fx0A - LD Vx, K - Wait for a key press, store the value of the key in Vx
-            elif n = 0x0A:
+            elif n == 0x0A:
                 self.reg_V[x] = self.wait_for_key()
             # Fx15 - LD DT, Vx - Set delay timer = Vx
-            elif n = 0x15:
+            elif n == 0x15:
                 self.reg_DT = self.reg_V[x]
             # Fx18 - LD ST, Vx - Set sound timer = Vx
-            elif n = 0x18:
+            elif n == 0x18:
                 self.reg_ST = self.reg_V[x]
             # Fx1E - ADD I, Vx - Set I = I + Vx
-            elif n = 0x1E:
+            elif n == 0x1E:
                 self.reg_VI += self.reg_V[x]
             # Fx29 - LD F, Vx - Set I = location of sprite for digit Vx
-            elif n = 0x29:
+            elif n == 0x29:
                 self.reg_VI = self.reg_V[x] * 5
             # Fx33 - LD B, Vx - Store BCD representation of Vx in memory locations I, I+1, and I+2
-            elif n = 0x33:
+            elif n == 0x33:
                 self.memory[self.reg_VI]     = self.reg_V[x] / 100;
                 self.memory[self.reg_VI + 1] = self.reg_V[x] % 100 / 10;
                 self.memory[self.reg_VI + 2] = self.reg_V[x] % 10;
             # Fx55 - LD [I], Vx - Store registers V0 through Vx in memory starting at location I
-            elif n = 0x55:
+            elif n == 0x55:
                  for i in range(self._N_REGISTERS):
                     self.memory[self.reg_VI + i] = self.reg_V[i]
             # Fx65 - LD Vx, [I] - Read registers V0 through Vx from memory starting at location I
-            elif n = 0x65:
+            elif n == 0x65:
                 for i in range(self._N_REGISTERS):
                      self.reg_V[i] = self.memory[self.reg_VI + i]
         self.reg_PC += 2
 
 
-    def draw_sprite(self):
-        raise NotImplementedError
+    def draw_sprite(self, x, y, n):
+        sprite = self.memory[self.reg_VI: self.reg_VI+n]
+        for row, byte in enumerate(sprite):
+            for col in range(7, -1, -1):
+                bit = (byte >> col) & 1
+                idx = np.ravel_multi_index(np.array([x + row, y + 7 - col]), self._DISPLAY_SIZE, mode='wrap')
+                if (bit and self.video_memory[idx]):
+                    V[0xF] = 1;
+                self.video_memory[idx] ^= bit
+
 
     def update_screen(self):
-        raise NotImplementedError
+        self.display.draw(self.video_memory)
+
+
+class Display(object):
+
+    def __init__(self, shape, title='Chip8 - Emulator'):
+        self.shape = shape
+        pygame.init()
+        window = pygame.display.set_caption(title)
+        self.canvas = pygame.display.set_mode( (64, 32) )
+        self.canvas.fill((0x99, 0xBD, 0x2A))
+        pygame.display.update()
+        
+
+    def draw(self, image):
+        with np.nditer([image.reshape((64,32))], ['multi_index'], [['readonly']]) as it:
+            while not it.finished:
+                if it[0][...] == 1:
+                    pygame.draw.rect(
+                        self.canvas,
+                        (0x00, 0x00, 0x00),
+                        (it.multi_index[1], it.multi_index[0], 1, 1),
+                        0
+                    )
+                it.iternext()
+        pygame.display.update()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+
+if __name__ == '__main__':
+    shape = (64, 32)
+    display = Display(shape=shape)
+    cpu = Cpu(display=display)
+    cpu.draw_sprite(0,0,10)
+    cpu.update_screen()
+    display.run()
